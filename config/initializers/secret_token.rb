2ea8ee0ce01b0f5df89ed9ab05f76e02e049c367
Rob W <rob.whitehead@gmail.com>
2014-01-31 14:40:05 -0800

    Add ZURB Foundation content, working products toolbar and scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'a64a2fa52117f7fbf15fbff8da0c4cec29c3ceec10b4eb81c88d70f92f1bd13a443ea5069da94107623c3f9f52900f6b4224b2e8582ce06e192a3755467ffd9b'
